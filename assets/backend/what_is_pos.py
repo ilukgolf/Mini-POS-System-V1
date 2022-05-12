c336f5b8af5bc5a58310fc5d2737ebd1c042f7d7
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 20:51:26 +0700

    Create what_is_pos.py

---
+++ assets/backend/what_is_pos.py
@@ -0,0 +1,3 @@
+import tkinter.messagebox as tkmsg
+def Hmmm():
+    tkmsg.showinfo("HELP - What is POS", "Point Of Sale ? No no no.\nPOS is The Power Of Snake.\nThank you for using my POS :)\n\nFor help, please contact me at:\nEmail: jirawat.bunm@bumail.net")
