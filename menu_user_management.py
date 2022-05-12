6aa96381dc420daf255520be544fddab33e9e82e
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 23:17:58 +0700

    Update menu_user_management.py

--- menu_user_management.py
+++ menu_user_management.py
@@ -21,24 +21,27 @@ def user_management(display, user_data):
         show_user_account_frame = tk.Frame(main_frame, bg=config.color_red)
         show_user_account_frame.place(x=5, y=5, width=1037, height=610)
 
-        # import connect_database
-        # sql = "SELECT * FROM user_accounts"
-        # user_account_list = connect_database.database_execute(sql)
+        import connect_database
+        sql = "SELECT * FROM user_accounts"
+        user_account_list = connect_database.database_execute(sql)
 
-        # print(user_account_list)
+        print(user_account_list)
 
-        # top_frame = tk.Frame(show_user_account_frame, bg=config.color_green)
-        # top_frame.place(x=0, y=0, width=1037, height=50)
+        top_frame = tk.Frame(show_user_account_frame, bg=config.color_default)
+        top_frame.place(x=0, y=0, width=1037, height=50)
 
-        # add_new_user_button = tk.Button(
-        #     top_frame, text=" ออกจากระบบ ", cursor="hand2", image=logout_icon, compound=tk.LEFT,
-        #     activebackground=config.color_red, activeforeground=config.color_white,
-        #     font=tkfont.Font(family='FC Lamoon', size=14, weight='bold'),
-        #     background=config.color_red, foreground=config.color_white,
-        #     command=lambda: logout_button_click())
-        # add_new_user_button.grid(row=1, column=0, columnspan=1, rowspan=1, padx=5, pady=0, sticky=tk.E)
+        add_new_user_button = tk.Button(
+            top_frame, text=" เพิ่มบัญชีผู้ใช้ ", cursor="hand2", compound=tk.LEFT,
+            activebackground=config.color_green, activeforeground=config.color_white,
+            font=tkfont.Font(family='FC Lamoon', size=14, weight='bold'),
+            background=config.color_green, foreground=config.color_white,
+            command=lambda: add_new_user_account())
+        add_new_user_button.grid(row=1, column=0, columnspan=1, rowspan=1, padx=5, pady=5, sticky=tk.E)
 
     def show_user_account_detail():
+        pass
+
+    def add_new_user_account():
         pass
 
     create_frame()
