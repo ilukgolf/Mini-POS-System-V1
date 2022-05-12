b4f2c840dc06559664eda87d844ce4cc9e291e69
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 23:17:57 +0700

    Update menu_summary_report.py

--- menu_summary_report.py
+++ menu_summary_report.py
@@ -4,8 +4,11 @@ import config
 def summary_report(display, user_data):
     summary_report_frame = tk.Frame(display, bg=config.color_default)
 
-    #! IMPORTANT: Create a new frame over order_product_frame by pack grid or place.
-    #! IMPORTANT: And write everything on frame you create.
+    # ! IMPORTANT: Create a new frame over order_product_frame by pack grid or place.
+    # ! IMPORTANT: And write everything on frame you create.
+
+    test_label = tk.Label(summary_report_frame, text="Test", bg=config.color_default)
+    test_label.pack(expand=True, fill="both")
 
     return summary_report_frame
 
