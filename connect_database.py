6a5b8b62cc155cb164ffcfc801a849576bff1f9a
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 23:03:16 +0700

    Update connect_database.py

--- connect_database.py
+++ connect_database.py
@@ -9,6 +9,14 @@ def database_execute(sql):
     conn.close()
     return data_fetched
 
+def insert_data(sql):
+    global conn, cursor
+    conn = sqlite3.connect('assets\\backend\\database\\minipos.db')
+    cursor = conn.cursor()
+    cursor.execute(sql, )
+    conn.commit()
+    conn.close()
+
 if __name__ == '__main__':
     import main
     main.App()
