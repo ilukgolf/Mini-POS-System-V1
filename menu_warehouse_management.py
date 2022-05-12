fcb1ac957218cbeb58ceef865ca9acd3b2e022fd
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 20:50:49 +0700

    Create menu_warehouse_management.py

---
+++ menu_warehouse_management.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import config
+
+def warehouse_management(display, user_data):
+    warehouse_management_frame = tk.Frame(display, bg=config.color_default)
+
+    #! IMPORTANT: Create a new frame over order_product_frame by pack grid or place.
+    #! IMPORTANT: And write everything on frame you create.
+
+    return warehouse_management_frame
+
+if __name__ == '__main__':
+    import main
+    main.App()
