da74cafc2ded79456e8b50daf32f1648e8e913ce
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 20:50:59 +0700

    Create menu_search_product.py

---
+++ menu_search_product.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import config
+
+def search_product(display, user_data):
+    search_product_frame = tk.Frame(display, bg=config.color_default)
+
+    #! IMPORTANT: Create a new frame over order_product_frame by pack grid or place.
+    #! IMPORTANT: And write everything on frame you create.
+
+    return search_product_frame
+
+if __name__ == '__main__':
+    import main
+    main.App()
