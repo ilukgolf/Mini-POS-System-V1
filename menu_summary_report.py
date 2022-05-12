8a3d5f61ee4ba15c1210ec6e3c55e9b94e5df426
ilukgolf <jgbbmrssk1909@outlook.com>
2022-05-12 20:50:54 +0700

    Create menu_summary_report.py

---
+++ menu_summary_report.py
@@ -0,0 +1,14 @@
+import tkinter as tk
+import config
+
+def summary_report(display, user_data):
+    summary_report_frame = tk.Frame(display, bg=config.color_default)
+
+    #! IMPORTANT: Create a new frame over order_product_frame by pack grid or place.
+    #! IMPORTANT: And write everything on frame you create.
+
+    return summary_report_frame
+
+if __name__ == '__main__':
+    import main
+    main.App()
